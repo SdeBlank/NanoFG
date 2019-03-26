f7fbb16af915dc7c91056fc8eb50ffa94edf5a50
Mark van Roosmalen <mroosmalen@cog146.op.umcutrecht.nl>
2019-03-26 11:19:00 +0100

    Add gene annotation

--- fusion_gene_read_extraction.py
+++ fusion_gene_read_extraction.py
@@ -4,6 +4,8 @@ import argparse
 import vcf as pyvcf
 import pysam
 import sys
+import requests
+import json
 
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(description='Put here a description.')
@@ -12,10 +14,43 @@ parser.add_argument('-v', '--vcf', required=True, type=str, help='Input NanoSV v
 
 args = parser.parse_args()
 
-def create_fasta( chr, start, end, svid, exclude):
-    print( chr, start, end )
+def get_gene_overlap( chr, pos, ori, bp ):
+    SERVER="https://GRCh37.rest.ensembl.org/overlap/region/"
+    SPECIES="human"
+    HEADERS={"Content-Type" : "application/json"}
+    FEATURE="gene"
+    genes_request=requests.get(SERVER+SPECIES+"/"+str(chr)+":"+str(pos)+"-"+str(pos)+"?feature="+FEATURE, headers=HEADERS)
+    genes_response = genes_request.text
+    genes_data=json.loads(genes_response)
+
+    fusions = dict()
+
+    for gene in genes_data:
+        if gene["biotype"]=="protein_coding":
+            if not ori and gene['strand'] == 1:
+                if 'donor' not in fusions:
+                    fusions['donor'] = dict()
+                fusions['donor'][gene['id']+"\t"+bp] = gene['start']
+            elif not ori and gene['strand'] == -1:
+                if 'acceptor' not in fusions:
+                    fusions['acceptor'] = dict()
+                fusions['acceptor'][gene['id']+"\t"+bp] = gene['start']
+            elif ori and gene['strand'] == 1:
+                if 'acceptor' not in fusions:
+                    fusions['acceptor'] = dict()
+                fusions['acceptor'][gene['id']+"\t"+bp] = gene['end']
+            elif ori and gene['strand'] == -1:
+                if 'donor' not in fusions:
+                    fusions['donor'] = dict()
+                fusions['donor'][gene['id']+"\t"+bp] = gene['end']
+
+    return( fusions )
+
+def create_fasta( chr, start, end, svid, exclude, fusion ):
+    if end < start:
+        end, start = start, end
     bamfile = pysam.AlignmentFile(args.bam, "rb" )
-    fasta = open(svid+".fasta", 'a+')
+    fasta = open(svid+"_"+fusion+".fasta", 'a+')
     for read in bamfile.fetch(chr, start, end):
         if read.query_name in exclude or read.seq == None:
             continue
@@ -26,6 +61,33 @@ def create_fasta( chr, start, end, svid, exclude):
 
 vcf_reader = pyvcf.Reader(open(args.vcf, 'r'))
 for record in vcf_reader:
-    create_fasta(record.CHROM, record.POS-40000, record.POS, record.ID, record.INFO['REF_READ_IDS_1'])
-    create_fasta(record.ALT[0].chr, record.ALT[0].pos, record.ALT[0].pos+1000, record.ID, record.INFO['REF_READ_IDS_2'])
-    break
+    print( record )
+    fusions = get_gene_overlap(record.CHROM, record.POS, record.ALT[0].orientation, '1' )
+    fusions.update(get_gene_overlap(record.ALT[0].chr, record.ALT[0].pos, record.ALT[0].remoteOrientation, '2' ))
+    print( fusions )
+    if 'donor' in fusions and 'acceptor' in fusions:
+        for donor in fusions['donor']:
+            donor_gene, donor_bp = donor.split("\t")
+            for acceptor in fusions['acceptor']:
+                acceptor_gene, acceptor_bp = acceptor.split("\t")
+                fusion = donor_gene+"_"+acceptor_gene
+                if donor_bp == '1' and acceptor_bp == '2':
+                    donor_chr = record.CHROM
+                    donor_start = fusions['donor'][donor]
+                    donor_end = record.POS
+                    create_fasta(donor_chr, donor_start, donor_end, record.ID, record.INFO['REF_READ_IDS_1'], fusion)
+                    acceptor_chr = record.ALT[0].chr
+                    acceptor_start = record.ALT[0].pos
+                    acceptor_end = fusions['acceptor'][acceptor]
+                    create_fasta(acceptor_chr, acceptor_start, acceptor_end, record.ID, record.INFO['REF_READ_IDS_2'], fusion)
+                elif donor_bp == '2' and acceptor_bp == '1':
+                    donor_chr = record.ALT[0].chr
+                    donor_start = fusions['donor'][donor]
+                    donor_end = record.ALT[0].pos
+                    create_fasta(donor_chr, donor_start, donor_end, record.ID, record.INFO['REF_READ_IDS_2'], fusion)
+                    acceptor_chr = record.CHROM
+                    acceptor_start = fusions['acceptor'][acceptor]
+                    acceptor_end = record.POS
+                    create_fasta(acceptor_chr, acceptor_start, acceptor_end, record.ID, record.INFO['REF_READ_IDS_1'], fusion)
+    #create_fasta(record.ALT[0].chr, record.ALT[0].pos, record.ALT[0].pos+1000, record.ID, record.INFO['REF_READ_IDS_2'])
+    #break
